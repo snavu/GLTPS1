697c646617eb297dd913f319a11fa48945495855
tempqwq <cktran02@gmail.com>
2023-03-24 21:38:42 -0700

    Implement bullet hole decals

--- Assets/Assets/Scripts/Character/PlayeGunController.cs
+++ Assets/Assets/Scripts/Character/PlayeGunController.cs
@@ -11,11 +11,7 @@ public class PlayeGunController : MonoBehaviour
     [SerializeField]
     private Camera camera;
     [SerializeField]
-    private RectTransform canvasRectTransform;
-    [SerializeField]
-
-    private float raycastOffset;
-
+    private GameObject bulletHoleDecal;
     void Start()
     {
         playerInputScript.actions.Player.Fire.performed += Fire;
@@ -34,23 +30,28 @@ public class PlayeGunController : MonoBehaviour
             {
                 // Determine the direction of the bullet
                 Vector3 bulletDirection = (hit.point - muzzle.position).normalized;
+
+                //instantiate decal 
+                //note: minus hit.normal to spawn position to offset the decal object into the mesh along direction of normal and prevent z-fighting
+                Quaternion newBulletHoleDecalRotation = Quaternion.LookRotation(hit.normal, Vector3.up);
+                GameObject newBulletHoleDecal = Instantiate(bulletHoleDecal, hit.point - (hit.normal * 0.1f), newBulletHoleDecalRotation);
             }
         }
-    }
 
-    void OnDrawGizmos()
-    {
-        // Cast a ray from the muzzle position to the center of the screen
-        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        void OnDrawGizmos()
+        {
+            // Cast a ray from the muzzle position to the center of the screen
+            Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-        // Check if the ray hits something
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit);
+            // Check if the ray hits something
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
 
-        // Determine the direction of the bullet
-        Vector3 bulletDirection = (hit.point - muzzle.position).normalized;
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(muzzle.position, bulletDirection * 100);
+            // Determine the direction of the bullet
+            Vector3 bulletDirection = (hit.point - muzzle.position).normalized;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(muzzle.position, bulletDirection * 100);
+        }
     }
 }
 

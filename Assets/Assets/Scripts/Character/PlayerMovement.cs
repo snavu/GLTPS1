6145da84a9982674346bb59294acc816bbdfb7b9
tempqwq <cktran02@gmail.com>
2023-04-15 18:37:13 -0700

    Make HUD more transparent

--- Assets/Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Assets/Scripts/Character/PlayerMovement.cs
@@ -20,7 +20,6 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 4.0f;
     public float runSpeed = 7.0f;
     [SerializeField] float slowMovementRate = 0.5f;
-    [SerializeField] float lowEnergyMovementThreshold;
     public float ADSSpeed = 2.0f;
 
     [SerializeField] private float gravity = -9.81f;
@@ -47,7 +46,6 @@ public class PlayerMovement : MonoBehaviour
 
         //set layermask interact with vehicle and default
         layerMask = LayerMask.GetMask("Default", "Vehicle");
-
     }
 
     void OnDisable()
